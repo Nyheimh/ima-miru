a136f1191ab3ef0d44f153245b217d96e7bc40fe
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-04-02 11:55:48 -0700

    Deleted unnecessary folders and updated the routes

--- routes/shows.js
+++ routes/shows.js
@@ -1,7 +1,14 @@
 const { Router } = require('express')
 const controllers = require('../controllers/shows')
-const router = Router()
+const restrict = require('../helpers/restrict')
 
-router.get('/shows', controllers.getShows)
+const router = Router()
+router.get('/', controllers.getShows)
+router.get('/:id', controllers.getShow)
+// restricted
+router.post('/',restrict, controllers.createShow)
+router.put('/:id', restrict, controllers.updateShow)
+router.delete('/:id', restrict, controllers.deleteShow)
+
 
 module.exports = router
