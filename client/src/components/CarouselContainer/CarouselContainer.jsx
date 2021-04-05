b0cb9f890367077317fb2c45e4ff52dde0640b30
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-05 13:30:18 -0700

    images updated

--- client/src/components/CarouselContainer/CarouselContainer.jsx
+++ client/src/components/CarouselContainer/CarouselContainer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import './CarouselContainer.css'
+// import { Link } from 'react-router-dom'
+
 
 const CarouselContainer = () => {
     return (
@@ -8,12 +10,12 @@ const CarouselContainer = () => {
         <Carousel.Item >
         <img
           className="d-block w-100"
-          src="https://i.pinimg.com/originals/9e/c4/25/9ec425ae13b13c398936765251e27766.jpg"
+          src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Ffilmdaily.co%2Fwp-content%2Fuploads%2F2020%2F10%2Fboys-over-flowers-anime_lede.jpg"
           alt="First slide"
-        />
+          />
         <Carousel.Caption>
-          <h3>Boys Over Flowers</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          {/* <h3>Boys Over Flowers</h3> */}
+          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -23,30 +25,30 @@ const CarouselContainer = () => {
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h3>Hunter x Hunter</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* <h3>Hunter x Hunter</h3> */}
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.pinimg.com/originals/31/7f/fd/317ffdfd07542eb695117161b1c753c9.jpg"
+          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e858f0b0-1c68-4459-9031-a39f28fe37bf/dd8958t-09d667a0-cc0d-46f6-b34e-83ec5279bdc2.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZTg1OGYwYjAtMWM2OC00NDU5LTkwMzEtYTM5ZjI4ZmUzN2JmXC9kZDg5NTh0LTA5ZDY2N2EwLWNjMGQtNDZmNi1iMzRlLTgzZWM1Mjc5YmRjMi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.NhD5Vsyrh5-lojWDkuY0steAUvBeUSDq_KwrQF2VwSc"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Dragon Ball Z</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          {/* <h3>Dragon Ball Z</h3> */}
+          {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
         </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://cdn11.bigcommerce.com/s-b72t4x/images/stencil/1280x1280/products/11169/20874/24_1075_Naruto_Group__97803.1415650378.jpg?c=2"
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PYswdoFN0qdTRqTcYqLtDwAAAA%26pid%3DApi&f=1"
           alt="Fourth slide"
         />
         <Carousel.Caption>
-          <h3>Naruto</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          {/* <h3>Naruto</h3> */}
+          {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
