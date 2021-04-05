bcea442d39834e823c8be6e97813ab1f344aea09
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-04-05 11:29:37 -0400

    sign in scree ncomplete

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -12,3 +12,5 @@ const SignUp = () => {
 }
 
 export default SignUp
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImlybXoiLCJlbWFpbCI6ImlybXpAZ21haWwuY29tIiwiaWF0IjoxNjE3NjM1MjE0fQ.dFcRIKD4hx0eGOHtdjIgItxcHXtsI1ZprN--9q0sB5E
