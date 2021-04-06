caa1585c1f3b822255b66038fdc3a7db00059e95
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-05 20:13:55 -0700

    updated

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+      <footer class="text-center text-white" style="background-color: #f1f1f1;">
+      <div class="container pt-4">
+        <section class="mb-4">
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-twitter"></i
+          ></a>
+          {/* <!-- Linkedin --> */}
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-linkedin"></i
+          ></a>
+          {/* <!-- Github --> */}
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-github"></i
+          ></a>
+        </section>
+      </div>
+      <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:Ima Miru
+        <a class="text-dark" href="https://github.com/Jordanm96/ima-miru">Github</a>
+      </div>
+    </footer>
     )
 }
 
