cd6034d0c72d46f52fa951c048737069f5561286
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-04-06 12:06:19 -0400

    changed node version to 15.8.0

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -4,22 +4,24 @@ import CarouselContainer from '../../components/CarouselContainer/CarouselContai
 import { getShows } from '../../services/shows'
 
 const Home = ({user}) => {
-    const [allShows, setAllShows] = useState([])
+//     const [allShows, setAllShows] = useState([])
 
-useEffect(() => {
-    const fetchShows = async () => {
-      const shows = await getShows()
-      setAllShows(shows)
-    }
-    fetchShows()
-  }, [])
+// useEffect(() => {
+//     const fetchShows = async () => {
+//       const shows = await getShows()
+//       setAllShows(shows)
+//     }
+//     fetchShows()
+//   }, [])
 
-console.log(allShows)
+// console.log(allShows)
 
     return (
         <div>
             <Layout user={user}>
-                <CarouselContainer allShows={allShows}/>
+                {/* <CarouselContainer allShows={allShows}/> */}
+                <CarouselContainer/>
+
             </Layout>
         </div>
     )
