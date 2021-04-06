66a5db40a2b9d464a99f1629b482831a94e86813
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-04-05 17:18:03 -0700

    Basic Sign In formatting complete

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,88 +1,82 @@
-import React, {useState} from 'react'
-import './SignIn.css'
-import { signIn } from '../../services/users'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from "react";
+import { signIn } from "../../services/users";
+import { useHistory } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./SignIn.css";
+import Layout from "../../components/shared/Layout/Layout";
+import { Button, Form, FormGroup, Label, Input } from "react-bootstrap";
 
 const SignIn = (props) => {
-    const history = useHistory()
+  const history = useHistory();
 
-    const [form, setForm] = useState({
-        username: '',
-        password: '',
-        isError: false,
-        errorMsg: ''
-    })
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+    isError: false,
+    errorMsg: "",
+  });
 
-    const handleChange = event => {
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onSignIn = (event) => {
+    event.preventDefault();
+
+    const { setUser } = props;
+
+    signIn(form)
+      .then((user) => {
+        setUser(user);
+      })
+      .then(() => history.push("/"))
+      .catch((error) => {
+        console.error(error);
         setForm({
-            ...form,
-            [event.target.name]: event.target.value
-        })
+          isError: true,
+          errorMsg: "Invalid Credentials",
+          username: "",
+          password: "",
+        });
+      });
+  };
+
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return <button type="submit">Sign In</button>;
     }
+  };
 
-    const onSignIn = event => {
-        event.preventDefault()
+  const { username, password } = form;
 
-        const { setUser } = props
+  return (
+    <Layout user={props.user}>
+    <Form className="signIn-form-container" onSubmit = {onSignIn}>
+      <h2>Welcome</h2>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Username</Form.Label>
+          <Form.Control required type="text" name="username" value={username} placeholder="Username" onChange={handleChange}/>
+      </Form.Group>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control required name="password" value={password} type="password" placeholder="Password" onChange={handleChange} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign In
+      </Button>
+    </Form>
+  </Layout>
+  );
+};
 
-        signIn(form)
-            .then(user => {
-                setUser(user)
-            })
-            .then(() => history.push('/'))
-            .catch(error => {
-                console.error(error)
-                setForm({
-                    isError: true,
-                    errorMsg: 'Invalid Credentials',
-                    username: '',
-                    password: ''
-                })
-            })
-    }
-
-    const renderError = () => {
-        const toggleForm = form.isError ? 'danger' : ''
-        if (form.isError) {
-            return (
-                <button type="submit" className={toggleForm}>
-                    {form.errorMsg}
-                </button>
-            )
-        } else {
-            return <button type="submit">Sign In</button>
-        }
-    }
-
-    const { username, password } = form
-
-    return (
-        <div className="form-container">
-            <h3>Sign In</h3>
-            <form onSubmit={onSignIn}>
-                <label>Username</label>
-                <input
-                    required
-                    type="text"
-                    name="username"
-                    value={username}
-                    placeholder="Enter Username"
-                    onChange={handleChange}
-                />
-                <label>Password</label>
-                <input
-                    required
-                    name="password"
-                    value={password}
-                    type="password"
-                    placeholder="Password"
-                    onChange={handleChange}
-                />
-                {renderError()}
-            </form>
-        </div>
-    )
-}
-
-export default SignIn
-
+export default SignIn;
