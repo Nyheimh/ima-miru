9557ea753b1c7c2eac49db3ca5708ca6ec75706e
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-04-06 21:44:24 -0400

    home screen mobile css completed

--- client/src/components/CarouselContainer/CarouselContainer.jsx
+++ client/src/components/CarouselContainer/CarouselContainer.jsx
@@ -5,21 +5,7 @@ import './CarouselContainer.css'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css';
 
-const CarouselContainer = (props) => {
-  // const { allShows, allShows2, allShows3, setAllShows2, setAllShows3 } = props
-
-  // // useEffect (()=> {
-  // //   const spliceFunc = (shows, setShows, num) => {
-  // //     const move = shows.splice(0, num)
-  // //     console.log(move)
-  // //     setShows.push(...move)
-  // //     console.log(shows)
-  // //   }
-  // //   spliceFunc(allShows2, setAllShows2, 1)
-  // //   spliceFunc(allShows3, setAllShows3, 2)
-  // // },[])
-
-  // console.log(allShows)
+const CarouselContainer = ({allShows}) => {
 
   return (
     <div className="carousel">
@@ -97,69 +83,6 @@ const CarouselContainer = (props) => {
         />
       </Carousel>
     </div>
-
-    //   <Carousel className="carousel">
-    //     {/* {allShows.map(show => {
-    //         {console.log(show.title)}
-    //         <Carousel.Item>
-    //           <img
-    //             // className="d-block w-100"
-    //             className="d-none d-md-block"
-    //             src={show.imgURL}
-    //             alt={show.title}
-    //             />
-    //           <Carousel.Caption>
-    //           <p>{show.title}</p>
-    //           </Carousel.Caption>
-    //         </Carousel.Item>
-    //       })} */}
-
-    //     <Carousel.Item >
-    //     <div className="imageContainer" style={{ display: "flex", flexFlow: "row wrap", gap:"60px", width: "100%" }}>
-    //         <img
-    //           className="image"
-    //           // className="d-block w-100"
-    //           src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Ffilmdaily.co%2Fwp-content%2Fuploads%2F2020%2F10%2Fboys-over-flowers-anime_lede.jpg"
-    //           alt="First slide"
-    //         />
-    //         <img
-    //           className="image"
-    //           // className="d-none d-sm-inline w-100"
-    //           src="https://m.media-amazon.com/images/I/51wIwhIO6JL._AC_.jpg"
-    //           alt="Second slide"
-    //         />
-    //         <img
-    //           className="image"
-    //           // className="d-none d-md-inline w-100"
-    //           src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e858f0b0-1c68-4459-9031-a39f28fe37bf/dd8958t-09d667a0-cc0d-46f6-b34e-83ec5279bdc2.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZTg1OGYwYjAtMWM2OC00NDU5LTkwMzEtYTM5ZjI4ZmUzN2JmXC9kZDg5NTh0LTA5ZDY2N2EwLWNjMGQtNDZmNi1iMzRlLTgzZWM1Mjc5YmRjMi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.NhD5Vsyrh5-lojWDkuY0steAUvBeUSDq_KwrQF2VwSc"
-    //           alt="Third slide"
-    //         />
-    //       </div>
-    //     </Carousel.Item>
-    //     <Carousel.Item>
-    //     <div className="imageContainer" style={{ display: "flex", flexFlow: "row wrap", gap:"60px", width: "100%" }}>
-    //       <img
-    //       className="image"
-    //         // className="d-none d-sm-inline w-100"
-    //         src="https://m.media-amazon.com/images/I/51wIwhIO6JL._AC_.jpg"
-    //         alt="Second slide"
-    //       />
-    //       <img
-    //       className="image"
-    //         // className="d-none d-md-inline w-100"
-    //         src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e858f0b0-1c68-4459-9031-a39f28fe37bf/dd8958t-09d667a0-cc0d-46f6-b34e-83ec5279bdc2.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZTg1OGYwYjAtMWM2OC00NDU5LTkwMzEtYTM5ZjI4ZmUzN2JmXC9kZDg5NTh0LTA5ZDY2N2EwLWNjMGQtNDZmNi1iMzRlLTgzZWM1Mjc5YmRjMi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.NhD5Vsyrh5-lojWDkuY0steAUvBeUSDq_KwrQF2VwSc"
-    //         alt="Third slide"
-    //       />
-    //       <img
-    //       className="image"
-    //         // className="d-none d-lg-inline w-100"
-    //         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PYswdoFN0qdTRqTcYqLtDwAAAA%26pid%3DApi&f=1"
-    //         alt="Fourth slide"
-    //       />
-    //       </div>
-    //     </Carousel.Item>
-
-    //   </Carousel>
   )
 }
 
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -12,8 +12,6 @@ useEffect(() => {
     const fetchShows = async () => {
       const shows = await getShows()
       setAllShows(shows)
-      setAllShows2(shows)
-      setAllShows3(shows)
     }
     fetchShows()
   }, [])
@@ -23,13 +21,7 @@ console.log(allShows)
     return (
         <div>
             {allShows && <Layout user={user}>
-                {allShows && < CarouselContainer 
-                allShows={allShows}
-                allShows2={allShows2}
-                allShows3={allShows3}
-                setAllShows2={setAllShows2}
-                setAllShows3={setAllShows3}
-                />}
+                {allShows && < CarouselContainer allShows={allShows}/>}
                 {/* <CarouselContainer/> */}
             </Layout>}
         </div>
